52805b08ea4b73261198519bec6b80d73ba2ec04
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2013-10-18 16:58:37 -0400

    initial build

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <section class="top-container">
+
+            <div class="logo-container">
+
+                <div id="logo">
+
+                    <div class="logo-block b-white"></div>
+                    <div class="logo-block b-grey grey1"></div>
+                    <div class="logo-block b-salmon"></div>
+                    <div class="logo-block b-grey grey2"></div>
+                    <div class="logo-block b-red"></div>
+                    <div class="logo-block b-salmon s01"></div>
+                    <div class="logo-block b-salmon s02"></div>
+                    <div class="logo-block b-salmon s03"></div>
+                    <div class="logo-block b-salmon s04"></div> 
+
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <footer>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+
+        </footer>
+
+
+
+    </body>
+</html>
